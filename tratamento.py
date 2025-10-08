414c5df77b0cd8e39471ee6aaff9b57d63017344
natan_ls <lopesnatan147@gmail.com>
2025-10-08 10:18:23 -0300

    Adicionando o método que faz o tratamento dos nomes, e as listas com os nomes corretos

--- tratamento.py
+++ tratamento.py
@@ -1,5 +1,84 @@
 import pandas as pd
 from datetime import datetime
+from thefuzz import process
+
+"""
+    Variáveis
+"""
+
+nomes_mercados = [  'Azevedo', 'Bh Atacado', 'Bh Varejo', 'Bom Preço', 
+                    'Caribé', 'Caribé 2', 'Cestão Da Economia', 'Cestão Da Economia (Bairro)', 
+                    'Gordo', 'Kamila', 'Mart Minas', 'Montalvânia', 
+                    'Montalvânia 2', 'Nacional', 'Pag Pouco', 'Porto', 
+                    'Preço Baixo', 'Preço Baixo 2', 'Pão De Mel (Rodoviária)', 'Rocha', 
+                    'Super Maar', 'Terra Norte', 'União Mega Feira']
+
+nomes_postos_combustiveis = [   'Posto (1l)', 'Posto Alvorada (1l)', 'Posto Alvorada II (1l)', 'Posto BH (1l)', 
+                                'Posto Carrancas (1l)', 'Posto Central (1l)', 'Posto Jb Combustiveis (1l)', 'Posto Jb Combustiveis II (1l)', 
+                                'Posto Joelma (1l)', 'Posto Joelma (BH) (1l)', 'Posto Oliveira (1l)', 'Posto Oliveira II (1l)', 
+                                'Posto Paraguassú (1l)', 'Posto Pioneiro (1l)', 'Posto Vip (1l)']
+
+nomes_produtos = [  'Absorvente (8 unid.)', 'Alho (1kg)', 'Arroz (5kg)', 'Açúcar (5kg)', 
+                    'Batata (1kg)', 'Biscoito Maisena (pac. 200g)', 'Café / Em Pó (500g)', 'Carne De Primeira (1kg)', 
+                    'Carne De Segunda (1kg)', 'Cebola (1kg)', 'Creme Dental (Tubo 70g)', 'Creme Dental (Tubo 90g)', 
+                    'Desodorante Spray (100ml)', 'Detergente (500ml)', 'Extrato De Tomate (300g)', 'Extrato De Tomate (320g)', 
+                    'Extrato De Tomate (340g)', 'Extrato De Tomate (350g)', 'Farinha De Mandioca (1kg)', 'Farinha De Trigo (1kg)', 
+                    'Feijão (1kg)', 'Frango Resfriado Int. (1kg)', 'Fruta / Banana (1kg)', 'Leite (1l)', 
+                    'Leite Em Pó (400g)', 'Leite Em Pó (450g)', 'Linguiça Toscana Fresca (1kg)', 'Macarrão (pac. 500g)', 
+                    'Margarina (250g)', 'Ovos (1 dz.)', 'Papel Higiênico (pac. 4 unid.)', 'Pão Francês (1kg)', 
+                    'Queijo Mussarela Fat. (1kg)', 'Sabonete (85g)', 'Sabonete (90g)', 'Sabão Em Barra (unid.)', 
+                    'Sabão Em Pó (1kg)', 'Sabão Em Pó (400g)', 'Sabão Em Pó (450g)', 'Sabão Em Pó (500g)', 
+                    'Sabão Em Pó (600g)', 'Sabão Em Pó (800g)', 'Salsicha Avulsa (1kg)', 'Tomate (1kg)', 
+                    'Água Sanitária (1l)', 'Óleo De Soja (900ml)']
+
+nomes_marcas = [    '3 Corações', 'ABC', 'Acém', 'Agric./Familiar', 'Agrominas', 
+                    'Albany', 'Alcatra', 'Alegrete', 'Alho', 'All Lac', 
+                    'Always', 'Amafil', 'Amaral', 'Amália', 'Anchieta', 
+                    'Assim', 'Atual', 'Aurora', 'Ave Nova', 'Avivar', 
+                    'Aymoré', 'Azulim', 'BH', 'Batata Inglesa', 'Bauducco', 
+                    'Becel', 'Bento', 'Bica', 'Big Frango', 'Bob', 
+                    'Bom Gosto', 'Branco', 'Brilhante', 'Bruçúcar', 'Campo', 
+                    'Camponesa', 'Carinho', 'Carioca', 'Castor', 'Caturra', 
+                    'Cebola', 'Cemil', 'Charme', 'Classic', 'Claybom', 
+                    'Close Up', 'Cocal', 'Cocal Mirim', 'Codil', 'Codisul', 
+                    'Cofran', 'Cogran', 'Colgate', 'Colonial', 'Concórdia', 
+                    'Contra-filé', 'Coopatos', 'Corcovado', 'Cores (bax flower)', 'Coruripe', 
+                    'Costela', 'Cotochés', 'Coxão Duro', 'Coxão Mole', 'Coxão de Fora', 
+                    'Cristal de Minas', 'Código Premium', 'Da Dinha', 'Delta', 'Delícia', 
+                    'Dentil', 'Dona Benta', 'Dona Kuca', 'Dona Íris', 'Doriana', 
+                    'Dove', 'Ducampo', 'Eldorado', 'Elefante', 'Elite', 
+                    'Estrelux', 'Ferreira', 'Fino Grão', 'Flamboyant', 'Flor de Ypê', 
+                    'Floral', 'Fraldinha', 'Francis', 'Fredini', 'Friall', 
+                    'Frigoleste', 'Fugini', 'Galo', 'Gema de Minas', 'Gigante', 
+                    'Globo', 'Gostosão', 'Gran Petit', 'Granol', 'Intimus', 
+                    'Italac', 'Itambé', 'Kicaldo', 'Klipe', 'Lagarto', 
+                    'Laçúcar', 'Letícia', 'Limpol', 'Liza', 'Lopes', 
+                    'Lux', 'Mabel', 'Mais', 'Mara', 'Marilan', 
+                    'Marluce', 'Martins', 'Maçã do Peito', 'Mili', 'Mili Bianco', 
+                    'Milu', 'Mimo', 'Mimus', 'Minas +', 'Minuano', 
+                    'Monange', 'Mood', 'Músculo', 'Nestlé', 'Nivea', 
+                    'Norte de Minas', 'Nubiane', 'Nutril', 'Nutriway', 'Omo', 
+                    'Oral B', 'Pachá', 'Paladori', 'Paleta', 'Palistinha', 
+                    'Palmolive', 'Panela de Ouro', 'Papoula', 'Paradiso', 'Patinho', 
+                    'Patosul', 'Perdigão', 'Personal', 'Pif Paf', 'Pilão', 
+                    'Piracanjuba', 'Plus', 'Politriz', 'Polylar', 'Pomarola', 
+                    'Porte Alegre', 'Prata', 'Prata Solta', 'Predilecta', 'Premiata', 
+                    'Primor', 'Princesa', 'Promessa', 'Pão', 'Qboa', 
+                    'Qualy', 'Quatá', 'Quero', 'Racine', 'Razzo', 
+                    'Real', 'Rexona', 'Ribeirão', 'Rico', 'Rivelli', 
+                    'Saboroso', 'Sadia', 'Santa Amália', 'Santa Clara', 'Sapore', 
+                    'Saudali', 'Seara', 'Sem Marca', 'SempreLivre', 'Sepé', 
+                    'Serra Branca', 'Sorriso', 'Soya', 'Spa', 'Suaçuí', 
+                    'Sublime', 'Suinco', 'Super Globo', 'Supreme', 'Suíço', 
+                    'Sym', 'São João', 'Ta Barato', 'Tiaju', 'Tixan Ypê', 
+                    'Tomate', 'Triângulo', 'Tuff', 'Tunamã', 'Ultramais', 
+                    'UzziLim', 'Vale', 'Valor', 'Vasconcelos', 'Veleiro', 
+                    'Vermelho', 'Vida', 'Vilma', 'Vista Alegre', 'Vitaliv', 
+                    'Xap', 'Yara', 'Ypê', 'Zapel Plus']
+
+"""
+    Métodos
+"""
 
 def data_em_string(data):
     if type(data) == str:
@@ -81,6 +160,10 @@ def valida_moeda(moeda):
         
     return float(moeda)
 
+def corrigir_nome(nome, lista_nomes):
+    nome = nome.strip()
+
+    return process.extractOne(nome, lista_nomes)[0]
 
 def monta_df(df, nome_mercado, data):
     
@@ -119,4 +202,6 @@ def monta_df(df, nome_mercado, data):
                 continue
             
             
+            print(produto)
+
     return list_completo
